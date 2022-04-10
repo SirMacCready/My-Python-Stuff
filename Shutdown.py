1a988c1d369ee15eef22af9255c5811e012f1ac2
SirMacCready <102264892+SirMacCready@users.noreply.github.com>
2022-04-11 00:10:54 +0300

    Update Shutdown.py

--- Shutdown.py
+++ Shutdown.py
@@ -1,5 +1,6 @@
 import webbrowser, os , time
-
+print("Entrez la durée du compte à rebours en secondes : ")
+a = input()
 print('Début du compte à rebours')
-time.sleep(7200)
+time.sleep(a)
 os.system("shutdown /s /t 1")
